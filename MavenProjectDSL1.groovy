a9a423cf0d5545e6cebc90215261b4636ede316b
lavkumar809 <136153569+lavkumar809@users.noreply.github.com>
2023-08-16 18:57:17 +0530

    Update MavenProjectDSL1.groovy

--- MavenProjectDSL1.groovy
+++ MavenProjectDSL1.groovy
@@ -14,7 +14,8 @@ job('First-Maven-Project-Via-DSLjob') {
     
     steps {
         maven {
-            goals('clean', 'package') // Separate goals with a comma
+            goals(['clean', 'package']) // Pass goals as a list of strings
+            mavenInstallation('Maven-3.9.2')
         }
     }
 
